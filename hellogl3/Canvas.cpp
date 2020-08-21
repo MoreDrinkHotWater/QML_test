5878ca68a169128badb5106eb70d0beb31fcfb40
MoreDrinkHotWater <1401605907@qq.com>
2020-08-21 17:39:29 +0800

    temp code

--- hellogl3/Canvas.cpp
+++ hellogl3/Canvas.cpp
@@ -214,7 +214,7 @@ void Canvas::draw_centerLine(QPainter &painter)
         tolal_length += sqrt(pow(corner_line[i_1].x() - corner_line[i].x(),2) + pow(corner_line[i_1].y() - corner_line[i].y(),2));
     }
 
-//    float height = tolal_length/2;
+    float height = tolal_length/2;
 
     // 竖直垂线的长度
 //    float vertical_step =  height/(corner_line.size()/2);
@@ -231,8 +231,9 @@ void Canvas::draw_centerLine(QPainter &painter)
 
         if(i == 1)
         {
-            p1 = QPoint( min.x(), (min.y() + max.y())/2);
-            p2 = QPoint( max.x(), (min.y() + max.y())/2);
+            p1 = QPoint( corner_line[0].x(), corner_line[0].y() );
+            p2 = QPoint( corner_line[corner_line.size() - 1].x(), corner_line[corner_line.size() - 1].y());
+
             p1_p2_length = sqrt(pow(p2.x() - p1.x(), 2) + pow(p2.y() - p1.y(), 2));
             // 向量 P1_p2
             p1_p2 = QPoint( p2.x() - p1.x(), p2.y() - p1.y());
@@ -243,51 +244,84 @@ void Canvas::draw_centerLine(QPainter &painter)
 
         // 向量 P1_p2 的垂直向量 除以 (p1_p2_length/vertical_step) 是为了保证竖直垂线的长度一致
         p3_p4 = QPoint(-p1_p2.y()/(p1_p2_length/(vertical_step*i)), p1_p2.x()/(p1_p2_length/(vertical_step*i)));
+
+        std::cout<<"p3_p4: ("<<p3_p4.x()<<","<<p3_p4.y()<<")"<<std::endl;
+
         p3 = QPoint((p1.x() + p2.x())/2, (p1.y() + p2.y())/2);
+
+        std::cout<<"p3: ("<<p3.x()<<","<<p3.y()<<")"<<std::endl;
 
         //  把线段的中心点加入数组
         centerPoint_vector.push_back(p3);
 
         temp_centerPoint_vector.push_back(p3);
 
-#if 0
-        if(temp_centerPoint_vector.size() == 1)
-        {
-            p4 = QPoint(p3_p4.x() + p3.x(), p3_p4.y() + p3.y());
-            temp_vector.push_back(QLine(p3,p4));
-
-             p3_p4_length = sqrt(pow(p4.x() - p3.x(), 2) + pow(p4.y() - p3.y(), 2));
-             // 新的向量 p3_p4
-             p3_p4 = QPoint( p4.x() - p3.x(), p4.y() - p3.y());
-        }
-
-        // 连接两条线段的中点作延长线
-        if(temp_centerPoint_vector.size() == 2)
-        {
-            p3 = centerPoint_vector[0];
-            p4 = centerPoint_vector[1];
-
-            p3_p4_length = sqrt(pow(p4.x() - p3.x(), 2) + pow(p4.y() - p3.y(), 2));
-
-            //  延长线向量
-            p3_p4 = 2 * p3_p4_length * QPoint(p4.x() - p3.x() , p4.y() - p3.y());
-
-            p3 = p4;
-
-            p4 = QPoint(p3_p4.x() + p3.x(), p3_p4.y() + p3.y());
-
-            temp_vector.push_back(QLine(p3,p4));
-
-            temp_centerPoint_vector.pop_back();
-        }
-#endif
-
         p4 = QPoint(p3_p4.x() + p3.x(), p3_p4.y() + p3.y());
+
+        std::cout<<"p4: ("<<p4.x()<<","<<p4.y()<<")"<<std::endl;
+
         temp_vector.push_back(QLine(p3,p4));
 
         p3_p4_length = sqrt(pow(p4.x() - p3.x(), 2) + pow(p4.y() - p3.y(), 2));
-        // 新的向量 p3_p4
+
         p3_p4 = QPoint( p4.x() - p3.x(), p4.y() - p3.y());
+
+#if 0
+        // 连接两条线段的中点作延长线
+        if(temp_centerPoint_vector.size() == 2)
+        {
+
+            for(auto it : temp_centerPoint_vector)
+            {
+                std::cout<<"it: ("<<it.x()<<","<<it.y()<<")"<<std::endl;
+            }
+
+            p3 = temp_centerPoint_vector[0];
+            p4 = temp_centerPoint_vector[1];
+
+            p3_p4_length = sqrt(pow(p4.x() - p3.x(), 2) + pow(p4.y() - p3.y(), 2));
+
+            // 已知两点 p3, p4坐标,求两点连成的直线中的某一点坐标
+
+            float x = 2 * p4.x() - p3.x();
+
+            float y = 2 * p4.y() - p3.y();
+
+            std::cout<<"p5: ("<<x<<","<<y<<")"<<std::endl;
+
+            p3 = p4;
+
+            p4 = QPoint(x,y);
+
+            p3_p4 = QPoint( p4.x() - p3.x(), p4.y() - p3.y());
+
+            temp_vector.push_back(QLine(p3,p4));
+
+            temp_centerPoint_vector.pop_front();
+        }
+
+        // 已知一个直线方程  p3_p4: Ax + By + 1 = 0 ，求垂直于它的另一个直线方程
+        float B = (p3.x() * p4.x() - p3.x())/(p3.x() * p4.y() - p3.x() * p4.x() * p4.y());
+
+        float A = -(B * p3.y() +1)/p3.x();
+
+        std::cout<< "A: " <<A<<std::endl;
+
+        std::cout<< "B: " <<B<<std::endl;
+
+        QVector<QPoint> line_vec;
+        // 求向量 p3_p4 与琦角的交点
+        for (int j = 0; j < corner_line.size() - 1; j++) {
+
+            if(B * corner_line[j].x() - A * corner_line[j].y() + A * p4.y() - B * p4.x() == 0)
+            {
+                std::cout<<"corner_line[j]: ("<<corner_line[j].x()<<","<<corner_line[j].y()<<")"<<std::endl;
+
+                line_vec.push_back(QPoint(corner_line[j].x(), corner_line[j].y()));
+            }
+        }
+
+#elif 1
 
         QVector<QPoint> line_vec;
         // 求向量 p3_p4 与琦角的交点
@@ -297,13 +331,12 @@ void Canvas::draw_centerLine(QPainter &painter)
             {
                 line_vec.push_back(QPoint(corner_line[j].x(), corner_line[j].y()));
             }
-
             if(p4.y() < corner_line[j].y()  && p4.y() > corner_line[j+1].y())
             {
                 line_vec.push_back(QPoint(corner_line[j+1].x(), corner_line[j+1].y()));
             }
         }
-
+#endif
         if(line_vec.size() == 2)
         {
             p1 = line_vec[0];
@@ -316,15 +349,15 @@ void Canvas::draw_centerLine(QPainter &painter)
 
             p1_p2_length = parallel_step;
 
-//            p2 = QPoint(p1.x() + p1_p2.x(), p2.x() + p1_p2.y());
-
             temp_vector.push_back(QLine(p1,p2));
-
         }
     }
 
 #endif
     QVector<QLine> centerLine_vector;
+
+    std::cout<<"centerPoint_vector.size(): "<<centerPoint_vector.size()<<std::endl;
+
     for(int i = 0; i < centerPoint_vector.size() - 1; i++)
     {
         QLine temp_line = QLine(centerPoint_vector[i], centerPoint_vector[i+1]);
